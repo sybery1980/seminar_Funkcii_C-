8c4a2463efc0c6a87c8eedb0922fa40a2a7d3dcc
geekbrains <syberygames@gmail.com>
2022-10-17 22:26:08 +0300

    P5

---
+++ P5/Program.cs
@@ -0,0 +1,23 @@
+﻿int min = 10;
+int max = 99;
+int randomNumber = new Random().Next(min, max + 1);
+
+int firstDigit = randomNumber / 10;
+int secondDigit = randomNumber % 10;
+
+int maxDigit = Max(firstDigit, secondDigit);
+
+Console.WriteLine($"{randomNumber} -> {maxDigit}");
+
+
+int Max(int a, int b)
+{
+    if(a > b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
