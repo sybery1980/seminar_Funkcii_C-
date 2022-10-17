b6c20d64ea4f15e0eb78b811f934aef77361b60f
geekbrains <syberygames@gmail.com>
2022-10-17 22:27:22 +0300

    P7

---
+++ P7/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a % b == 0)
+{
+    Console.WriteLine("Кратное");
+
+}
+else
+{
+    Console.WriteLine(a % b);
+}
