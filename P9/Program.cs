a35d2257631844f7bf8938ca5e842b9b6cf4aee0
geekbrains <syberygames@gmail.com>
2022-10-17 22:28:26 +0300

    P9

---
+++ P9/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a * a == b || b * b == a)
+{
+    
+    Console.WriteLine("Да");
+
+}
+else
+{
+    Console.WriteLine("Нет");
+
+}
