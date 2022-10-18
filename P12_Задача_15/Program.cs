f035884323d8eefa4d6bcf144854c6bf3581c655
geekbrains <syberygames@gmail.com>
2022-10-18 21:46:32 +0300

    Задача 15

---
+++ P12_Задача_15/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+//и проверяет, является ли этот день выходным.
+
+
+Console.Write("Введите  число: ");
+int syb = Convert.ToInt32(Console.ReadLine());
+int weekend = 6;
+int max = 8;
+
+if(syb >= weekend && syb >= max  )
+    {
+        Console.WriteLine(" Недопустимое значение");
+    }
+        else if(syb < weekend)
+    { 
+        Console.WriteLine(" Будний день");
+    }             
+        else
+    {
+        Console.WriteLine(" Выходной день");
+    }
