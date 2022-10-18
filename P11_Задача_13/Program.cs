26a2b413430c636695a16d8b086939d617649454
geekbrains <syberygames@gmail.com>
2022-10-18 21:43:31 +0300

    Задача 13

---
+++ P11_Задача_13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 13:
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите  число: ");
+string syb = Convert.ToString(Console.ReadLine());
+
+    if(syb.Length >= 3)
+    {
+      Console.WriteLine(syb[2]);
+    } 
+        else
+        {    
+           Console.WriteLine("третьей цифры нет");
+        }
