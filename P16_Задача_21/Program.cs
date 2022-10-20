a6f90158dabf3b7d4f5e4de6acc5e7641d34d2ed
geekbrains <syberygames@gmail.com>
2022-10-20 21:49:10 +0300

    Задача_21_2

---
+++ P16_Задача_21/Program.cs
@@ -0,0 +1,15 @@
+﻿int x1 = ReadInt("Введите X точки A: ");
+int y1 = ReadInt("Введите Y точки A: ");
+int x2 = ReadInt("Введите X точки B: ");
+int y2 = ReadInt("Введите Y точки B: ");
+
+int sqrLengthX = (x2 - x1) * (x2 - x1);
+int sqrLengthY = (y2 - y1) * (y2 - y1);
+double distance = Math.Sqrt(sqrLengthX + sqrLengthY);
+Console.WriteLine("Расстояние равно " + distance);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
