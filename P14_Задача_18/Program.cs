767d53042bd26acd85f436ec1b8f64d2828b9450
geekbrains <syberygames@gmail.com>
2022-10-20 21:45:05 +0300

    Задача_18

---
+++ P14_Задача_18/Program.cs
@@ -0,0 +1,25 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+if(number < 1 || number > 4)
+{
+    Console.WriteLine("Такой четверти нет");
+    return;
+}
+if(number == 1)
+{
+    Console.WriteLine("x > 0 и y > 0");
+}
+
+else if(number == 2)
+{
+    Console.WriteLine("x < 0 и y > 0");
+}
+else if(number == 3)
+{
+    Console.WriteLine("x < 0 и y < 0");
+}
+
+else if(number == 4)
+{
+    Console.WriteLine("x > 0 и y < 0");
+    
+}
---
+++ P15_Задача_21/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите координаты точки А:");
+
+int ax = Convert.ToInt32(Console.ReadLine());
+int ay = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B:");
+
+int bx = Convert.ToInt32(Console.ReadLine());
+int by = Convert.ToInt32(Console.ReadLine());
+
+int sqrLength = (ax - bx ) * (ax - bx) + (ay - by) * (ay - by);
+
+double result = Math.Sqrt(sqrLength);
+Console.WriteLine(result);
