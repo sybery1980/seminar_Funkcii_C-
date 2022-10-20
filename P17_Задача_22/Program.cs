5cce01a45a7cb3d37003ff360fb057f4197b9728
geekbrains <syberygames@gmail.com>
2022-10-20 21:50:33 +0300

    Задача_22

---
+++ P17_Задача_22/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+if (number <= 0)
+{
+    number = - number;
+   
+}
+ while (count <= number)
+ {
+    Console.WriteLine(count * count);
+    count++;
+
+ }
